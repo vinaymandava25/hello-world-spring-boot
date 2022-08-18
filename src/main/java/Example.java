b0a49d3430c8afd95c21badc8c038d24d4a42da6
Vinay kumar <31988564+vinaymandava25@users.noreply.github.com>
2022-08-18 12:34:02 +0530

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World - v3!";
+        return "Hello World - Welcome to Vinay-azure";
     }
 
     public static void main(String[] args) throws Exception {
